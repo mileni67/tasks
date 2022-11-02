aa670e1b3748c74095940eb8887b5edb266edeab
Oleksandr Vykhor <sashavihor67@gmail.com>
2022-11-02 22:45:06 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,6 +10,19 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
     const render = () => {
         let htmlString = "";
 
@@ -17,32 +30,36 @@
             htmlString += `
             <li${task.done ? "style=\"text-decoration: line-through\"" : ""}
             >
+            <button class=".js-remove">usuń</button>
             ${task.content}
             </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
     };
 
-        const addNewTask = (newTaskContent) => {
-            tasks.push({
-                content: newTaskContent,
-            });
-            render();
-        };
 
-        const onFormSubmit =  (event) => {
-            event.preventDefault();
 
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-            
-            if(newTaskContent === "") {
-                return;
-            }
+    const onFormSubmit = (event) => {
+        event.preventDefault();
 
-           addNewTask(newTaskContent);
-        };
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
 
     const init = () => {
         render();
